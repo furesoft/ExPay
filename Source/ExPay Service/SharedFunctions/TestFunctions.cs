920fc4899fedca8c2a5ad83254b0fdfabe3ae619
Furesoft <filmee24@gmail.com>
2020-01-06 10:38:55 +0100

    initial commit

---
+++ Source/ExPay Service/SharedFunctions/TestFunctions.cs
@@ -0,0 +1,15 @@
+﻿using ExPay_Service.Dialogs;
+using Furesoft.Signals.Attributes;
+
+namespace ExPay_Service.SharedFunctions
+{
+    [Shared]
+    public class SharedUI
+    {
+        [SharedFunction(0x3A1)]
+        public static int OpenDialog()
+        {
+            return Utils.ShowDialog<int, TestDialog>();
+        }
+    }
+}
---
+++ Source/ExPay Service/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExPay_Service
+{
+    public static class Utils
+    {
+        [STAThread]
+        public static T ShowDialog<T, U>()
+            where U : Window, new()
+        {
+            var tcs = new TaskCompletionSource<T>();
+
+            var uiThread = new Thread(() =>
+            {
+                var a = new Application();
+                var dialog = new U();
+
+                a.MainWindow = dialog;
+                a.MainWindow.ShowDialog();
+
+                a.Exit += (s, e) =>
+                {
+                    tcs.SetResult((T)dialog.Tag);
+                };
+
+                a.Run();
+            });
+
+            uiThread.SetApartmentState(ApartmentState.STA);
+            uiThread.Start();
+
+            return tcs.Task.Result;
+        }
+    }
+}
