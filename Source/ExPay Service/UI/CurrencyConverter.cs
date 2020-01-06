e16b1294f16b7bfb944003269ac5e5fa8b26b21c
Furesoft <filmee24@gmail.com>
2020-01-06 19:59:29 +0100

    ui fixes

---
+++ Source/ExPay Service/UI/CurrencyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ExPay_Service.UI
+{
+    internal class CurrencyConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("{0:F2}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new CurrencyConverter();
+        }
+    }
+}
