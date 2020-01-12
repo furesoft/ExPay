d539fa854fb5e8419972e48cd0b3f094d5959f5c
Furesoft <filmee24@gmail.com>
2020-01-12 17:58:07 +0100

    added paydialog

---
+++ Source/ExPay Service/Dialogs/PayDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ExPay_Service.Dialogs
+{
+    public class PayDialog : Window
+    {
+        public PayDialog()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
