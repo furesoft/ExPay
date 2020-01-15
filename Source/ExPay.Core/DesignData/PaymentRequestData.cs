61706b49d2ee117a4dae8043b21ab5b86ee4b686
Furesoft <filmee24@gmail.com>
2020-01-15 16:30:51 +0100

    ui paydialog fix

---
+++ Source/ExPay.Core/DesignData/PaymentRequestData.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Markup.Xaml;
+using ExPay.Core.Models;
+using System;
+
+namespace ExPay.Core.DesignData
+{
+    public class PaymentRequestData : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var pr = new PaymentRequest();
+            pr.MerchantInfo = new PaymentMerchantInfo("Design Store");
+
+            return pr;
+        }
+    }
+}
