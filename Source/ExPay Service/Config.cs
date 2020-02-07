e771e08a48446286ec9e465349c35d27817729fe
Furesoft <filmee24@gmail.com>
2020-02-07 15:46:01 +0100

    added Config

---
+++ Source/ExPay Service/Config.cs
@@ -0,0 +1,38 @@
+﻿using DiscUtils.Registry;
+using System;
+using System.IO;
+
+namespace ExPay_Service
+{
+    public static class Config
+    {
+        public static RegistryKey AppConfig;
+        public static RegistryKey PaymentMethods;
+
+        public static RegistryHive Hive { get; private set; }
+
+        public static void Init()
+        {
+            if (File.Exists(ConfigFileName))
+            {
+                Hive = new RegistryHive(new FileStream(ConfigFileName, FileMode.OpenOrCreate));
+            }
+            else
+            {
+                Hive = RegistryHive.Create(ConfigFileName);
+            }
+
+            //init keys
+            PaymentMethods = Hive?.Root.CreateSubKey("PaymentMethods");
+            AppConfig = Hive?.Root.CreateSubKey("AppConfig");
+        }
+
+        public static void Reset()
+        {
+            File.Delete(ConfigFileName);
+            Init();
+        }
+
+        private static string ConfigFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "expay.config");
+    }
+}
