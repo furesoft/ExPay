a6ae28346a0d2123f9fbc25146c9c9357f7ccf0f
Furesoft <filmee24@gmail.com>
2020-02-21 13:03:01 +0100

    cleanup

--- Source/Plugins/TestPlugin/MsPayPaymentMethod.cs
+++ Source/Plugins/TestPlugin/MsPayPaymentMethod.cs
@@ -4,9 +4,9 @@ using System.Composition;
 namespace TestPlugin
 {
     [Export(typeof(IPaymentMethod))]
-    public class CreditCardPaymentMethod : IPaymentMethod
+    public class MsPayPaymentMethod : IPaymentMethod
     {
-        public PaymentMethodInfo Info => new PaymentMethodInfo("https://pay.microsoft.com/microsoftpay", "CreditCard", null);
+        public PaymentMethodInfo Info => new PaymentMethodInfo("https://pay.microsoft.com/microsoftpay", "Microsoft Pay", null);
 
         public void Initialize()
         {
