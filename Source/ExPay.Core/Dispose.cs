ea2107e742c433368901e8e88e52d72b85f8d01c
Furesoft <filmee24@gmail.com>
2020-02-21 12:52:57 +0100

    added automatic disposing

---
+++ Source/ExPay.Core/Dispose.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExPay.Core
+{
+    public static class Dispose
+    {
+        public static void Add(IDisposable disposable)
+        {
+            _disposables.Add(disposable);
+        }
+
+        public static void DisposeAll()
+        {
+            foreach (var d in _disposables)
+            {
+                d.Dispose();
+            }
+
+            _disposables.Clear();
+        }
+
+        private static List<IDisposable> _disposables = new List<IDisposable>();
+    }
+}
