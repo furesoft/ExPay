fa82c57ffe81e7220cc492d597d198639b45d66d
Furesoft <filmee24@gmail.com>
2020-02-27 21:36:22 +0100

    added adding pages to navigator

--- Source/ExPay.Core/Navigator.cs
+++ Source/ExPay.Core/Navigator.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using System.Collections.Generic;
 
 namespace ExPay_Service.Core
 {
@@ -7,7 +8,19 @@ namespace ExPay_Service.Core
         private static ContentControl _frame;
 
         public static int PageIndex = 0;
-        public static void Init(ContentControl frame, object defaultContent = null)
+        public static List<Control> Pages = new List<Control>();
+
+        public static void Forward()
+        {
+            if (PageIndex < Pages.Count - 1)
+            {
+                PageIndex++;
+
+                Navigate(Pages[PageIndex]);
+            }
+        }
+
+        public static void Init(ContentControl frame, Control defaultContent = null)
         {
             if (frame is null)
             {
@@ -19,6 +32,8 @@ namespace ExPay_Service.Core
             if (defaultContent != null)
             {
                 _frame.Content = defaultContent;
+
+                Pages.Add(defaultContent);
             }
         }
 
