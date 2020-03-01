9d00a6a6e990c7b32099f9d83985a1ef3d1b125e
Furesoft <filmee24@gmail.com>
2020-03-01 09:08:10 +0100

    added locales

--- Source/ExPay.Core/I18N.cs
+++ Source/ExPay.Core/I18N.cs
@@ -4,11 +4,12 @@ namespace ExPay.Core
 {
     public static class I18N
     {
+        public static string Domain = "ExPay";
         public static string _(string value)
         {
             return catalog.GetString(value);
         }
 
-        private static ICatalog catalog = new Catalog("Expay", "./Locale");
+        private static ICatalog catalog = new Catalog(Domain, "./Locale");
     }
 }
