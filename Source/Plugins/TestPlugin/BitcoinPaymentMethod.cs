d1abe7e7aa609999d81c798c8b84f76f3ff830f6
Furesoft <filmee24@gmail.com>
2020-03-01 10:16:46 +0100

    added nbitcoin to plugin

--- Source/Plugins/TestPlugin/BitcoinPaymentMethod.cs
+++ Source/Plugins/TestPlugin/BitcoinPaymentMethod.cs
@@ -1,4 +1,5 @@
 ﻿using ExPay.Core.Contracts;
+using NBitcoin;
 using System.Composition;
 using System.Threading.Tasks;
 
@@ -11,7 +12,16 @@ namespace TestPlugin
 
         public Task<object> BeforePay()
         {
+            var addr = GenerateAddress();
+
             return Task.FromResult<object>(new { hello = "world" });
+        }
+
+        string GenerateAddress()
+        {
+            var pubkey = ExtPubKey.Parse("xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz", Network.Main);
+            var newAddress = pubkey.Derive(0).Derive(0).PubKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main);
+            return newAddress.ToString();
         }
 
         public void Initialize()
