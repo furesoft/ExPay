aa2a53b0a2dabb4336c14caa2f0f61e3d33f9eae
Furesoft <filmee24@gmail.com>
2020-03-13 20:42:17 +0100

    added closedialog extension

---
+++ Source/ExPay.Core/UI/MarkupExtensions/CloseDialogExtension.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Markup.Xaml;
+using ExPay.Core.UI.Controls;
+using System;
+
+namespace ExPay.Core.UI.MarkupExtensions
+{
+    public class CloseDialogExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new DelegateCommand(_ => DialogService.CloseDialog());
+        }
+    }
+}
