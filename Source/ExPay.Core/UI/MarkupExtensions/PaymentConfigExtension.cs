5d12cd585f5689300d5b846ad6250df45fe0fa35
Furesoft <filmee24@gmail.com>
2020-03-13 20:25:59 +0100

    added paymentconfig extension

---
+++ Source/ExPay.Core/UI/MarkupExtensions/PaymentConfigExtension.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+
+namespace ExPay.Core.UI.MarkupExtensions
+{
+    public class PaymentConfigExtension : MarkupExtension
+    {
+        public PaymentConfigExtension(string key)
+        {
+            Key = key;
+        }
+
+        public string Key { get; set; }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return PaymentConfig.GetValue<object>(Key);
+        }
+    }
+}
