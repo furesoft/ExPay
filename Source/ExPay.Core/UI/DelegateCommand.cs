47d2db9ff664b1869b67d7887a7a525d6f53515c
Furesoft <filmee24@gmail.com>
2020-03-13 18:01:30 +0100

    added delegatecommand

---
+++ Source/ExPay.Core/UI/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ExPay.Core.UI
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public Action<object> handler;
+
+        public DelegateCommand(Action<object> handler)
+        {
+            this.handler = handler;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            handler?.Invoke(parameter);
+        }
+    }
+}
