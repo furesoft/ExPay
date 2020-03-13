44c3d447929fef06dffcf4346fb9d5384b5229cb
Furesoft <filmee24@gmail.com>
2020-03-13 17:08:08 +0100

    added PaymentStatusDialog

---
+++ Source/ExPay Service/Dialogs/PaymentStatusDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ExPay.Core;
+
+namespace ExPay_Service.Dialogs
+{
+    public class PaymentStatusDialog : UserControl
+    {
+        public PaymentStatusDialog()
+        {
+            this.InitializeComponent();
+
+            this.Initialized += FinishPayPage_Initialized;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void FinishPayPage_Initialized(object sender, System.EventArgs e)
+        {
+            this.FindControl<TextBlock>("statusLbl").Text = I18N._("Success");
+        }
+    }
+}
