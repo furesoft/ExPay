fbf5594f2a597bc1a7f2bc358dbcfcd30f98df0d
Furesoft <filmee24@gmail.com>
2020-03-13 18:01:06 +0100

    added opendialog markupextension

---
+++ Source/ExPay.Core/UI/MarkupExtensions/OpenDialogExtension.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ExPay.Core.UI.Controls;
+using System;
+
+namespace ExPay.Core.UI
+{
+    public class OpenDialogExtension : MarkupExtension
+    {
+        public OpenDialogExtension(string typename)
+        {
+            Typename = typename;
+        }
+
+        public string Typename { get; set; }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new DelegateCommand(_ =>
+            {
+                var type = Type.GetType(Typename);
+
+                if (type != null)
+                {
+                    var instance = Activator.CreateInstance(type);
+                    DialogService.OpenDialog((Control)instance);
+                }
+            });
+        }
+    }
+}
