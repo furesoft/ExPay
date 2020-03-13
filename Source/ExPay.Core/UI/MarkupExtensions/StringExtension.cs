0ed89f5aeded8f88c8fb882c5764c00730c1ab2c
Furesoft <filmee24@gmail.com>
2020-03-13 18:01:15 +0100

    added string markupextension

---
+++ Source/ExPay.Core/UI/MarkupExtensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+
+namespace ExPay.Core.UI.MarkupExtensions
+{
+    public class StringExtension : MarkupExtension
+    {
+        public StringExtension(string key)
+        {
+            Key = key;
+        }
+
+        public string Key { get; set; }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return I18N._(Key);
+        }
+    }
+}
